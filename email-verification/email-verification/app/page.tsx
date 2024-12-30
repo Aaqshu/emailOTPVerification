f53879c02f3cd9f2ab96d8634a123ee59b7317e4
Aaquib khan <aaquib4u@gmail.com>
2024-12-30 11:55:24 +0530

    Update page.tsx

--- email-verification/email-verification/app/page.tsx
+++ email-verification/email-verification/app/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { useState } from 'react'
 import { EmailForm } from './components/EmailForm'
 import { OTPForm } from './components/OTPForm'
@@ -20,4 +18,6 @@ export default function Home() {
     </div>
   )
 }
+
+
 
